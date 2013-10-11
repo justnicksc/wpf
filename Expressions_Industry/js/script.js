a948f6e850bd7c72640a97403c5e440989f7475e
justnicksc <nickwest@fullsail.edu>
2013-10-10 22:19:07 -0400

    added down payment

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -1,5 +1,6 @@
 // James West Expressions Industry project 10/10/13
 
 var fees = [.05, 389, 55,]; // sales fees 5 percent sales tax, $389 doc fee and $55 tag fee
-var carPrice = prompt("Enter the selling price of car" ,20000) // The price of the car before fees
-var downPayment = prompt (Enter down payment)
+var carPrice = prompt("Enter the selling price of car" ,20000); // The price of the car before fees
+var downPayment = prompt ("Enter down payment" ,carPrice *.20); // the down payment has a default value of 20 percent of selling price
+
