1c32050bad6778d2105bb83018661463ff14f43a
justnicksc <nickwest@fullsail.edu>
2013-10-10 21:32:32 -0400

    added givens to personal project

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -3,3 +3,8 @@
 
 //This script is going to calculate the average score for 5 rounds of golf
 
+var round1 = prompt("Enter round 1 score")
+var round2 = prompt("Enter round 2 score")
+var round3 = prompt("Enter round 3 score")
+var round4 = prompt("Enter round 4 score")
+var round5 = prompt("Enter round 5 score")
