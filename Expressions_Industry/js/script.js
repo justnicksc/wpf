f7e396eec93a8bbe213b7003e4c87e628982445b
justnicksc <nickwest@fullsail.edu>
2013-10-10 22:42:50 -0400

    moved if statement

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -5,10 +5,13 @@ var carPrice = prompt("Enter the selling price of car" ,20000); // The price of 
 var downPayment = prompt("Enter down payment" ,carPrice *.20); // the down payment has a default value of 20 percent of selling price
 var tradeValue = prompt("Enter trade in value" ,0);
 var taxes = carPrice * fees[0];
-var amountFinanced = carPrice - downPayment - tradeValue + taxes + fees[1] + fees[2];
+
 if (taxes > 300)
 {
     taxes = 300;
 }
+
+var amountFinanced = carPrice - downPayment - tradeValue + taxes + fees[1] + fees[2];
+
 
 alert ("The car cost " + carPrice + "\nThe taxes are " + taxes + "\nThe doc fee is " +fees[1] + "\nThe tag fee is " + fees[2] + "\n After down payment and trade in you will be financing " + amountFinanced);
