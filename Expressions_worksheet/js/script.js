87b1b273343b9e1343555d46260c176fb0e5a22a
justnicksc <nickwest@fullsail.edu>
2013-10-10 20:11:13 -0400

    added files and folders

---
+++ Expressions_worksheet/js/script.js
@@ -0,0 +1,10 @@
+// James West 10-10-2013
+
+
+// Dog years
+
+var sparkysAge = 1; // Sparky's age in dog years
+var dogYears = sparkysAge * 7 // Multiply Sparky's age by 7 to get his age in dog years
+
+console.log (sparkysAge)
+
