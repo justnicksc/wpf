7f1d76342773527a31248230769fe872afff0ccd
justnicksc <nickwest@fullsail.edu>
2013-10-10 23:08:39 -0400

    added comment

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -3,4 +3,7 @@
 var name = prompt ("enter your name"); //can enter full name or just first name
 var age = prompt ("enter your age"); //enter the age of the person
 var height = prompt ("enter your height"); //enter your height
-var averageGrowth = height / age;
+var averageGrowth = height / age; // gives the average amount grown each year of life
+
+alert
+
