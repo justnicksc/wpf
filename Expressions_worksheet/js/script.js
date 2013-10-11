de7b2679f1b01591cdc51ea4b9bbafd9b8cd169f
justnicksc <nickwest@fullsail.edu>
2013-10-10 20:19:08 -0400

    Dog years completed

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -4,7 +4,7 @@
 // Dog years
 
 var sparkysAge = 1; // Sparky's age in dog years
-var dogYears = sparkysAge * 7 // Multiply Sparky's age by 7 to get his age in dog years
+var dogYears = sparkysAge * 7; // Multiply Sparky's age by 7 to get his age in dog years
 
-console.log (sparkysAge)
+console.log (dogYears);
 
