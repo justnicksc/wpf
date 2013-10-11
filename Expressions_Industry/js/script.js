821da4958495da271e2c98fcd9ddd0fbf1cf5b43
justnicksc <nickwest@fullsail.edu>
2013-10-10 22:14:24 -0400

    added selling price var

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -1,3 +1,5 @@
 // James West Expressions Industry project 10/10/13
 
-var fees = [.05, 389, 55,]
+var fees = [.05, 389, 55,]; // sales fees 5 percent sales tax, $389 doc fee and $55 tag fee
+var carPrice = prompt(Enter the selling price of car) // The price of the car before fees
+var
--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -3,14 +3,14 @@
 
 //This script is going to calculate the average score for 4 rounds of golf
 
-var round1 = prompt("Enter round 1 score")// prompt to enter the score of the first round of golf
-var round2 = prompt("Enter round 2 score")// prompt to enter the score of your second round of golf
-var round3 = prompt("Enter round 3 score")// prompt to enter the score of your third round of golf
-var round4 = prompt("Enter round 4 score")// prompt to enter the score of your fourth round of golf
+var round1 = prompt("Enter round 1 score"); // prompt to enter the score of the first round of golf
+var round2 = prompt("Enter round 2 score"); // prompt to enter the score of your second round of golf
+var round3 = prompt("Enter round 3 score"); // prompt to enter the score of your third round of golf
+var round4 = prompt("Enter round 4 score"); // prompt to enter the score of your fourth round of golf
 
 
 // By adding the all of the scores together and dividing that total by the number of round we can get your average score
-var average = (Number(round1) + Number(round2) + Number(round3) + Number(round4))/4
+var average = (Number(round1) + Number(round2) + Number(round3) + Number(round4))/4;
 
 // the alert box will display the average
-alert ("I played 5 rounds of golf with an average score of " + average)
+alert ("I played 5 rounds of golf with an average score of " + average);
