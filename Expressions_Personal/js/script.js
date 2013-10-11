571782bb41064036c1d864b6e8dabc35fb60e5b6
justnicksc <nickwest@fullsail.edu>
2013-10-10 21:48:58 -0400

    edited comments
    I edited comments for variables

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -3,11 +3,13 @@
 
 //This script is going to calculate the average score for 5 rounds of golf
 
-var round1 = prompt("Enter round 1 score")// enter the score of the first round of golf
-var round2 = prompt("Enter round 2 score")// enter the score of your second round of golf
-var round3 = prompt("Enter round 3 score")// enter the score of your third round of golf
-var round4 = prompt("Enter round 4 score")// enter the score of your fourth round of golf
-var round5 = prompt("Enter round 5 score")
+var round1 = prompt("Enter round 1 score")// prompt to enter the score of the first round of golf
+var round2 = prompt("Enter round 2 score")// prompt to enter the score of your second round of golf
+var round3 = prompt("Enter round 3 score")// prompt to enter the score of your third round of golf
+var round4 = prompt("Enter round 4 score")// prompt to enter the score of your fourth round of golf
+var round5 = prompt("Enter round 5 score")// prompt to enter the score of your fifth round of golf
+
+
 var average = (Number(round1) + Number(round2) + Number(round3) + Number(round4) + Number(round5))/5
 
 alert ("I played 5 rounds of golf with an average score of " + average)
