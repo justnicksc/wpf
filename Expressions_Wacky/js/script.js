04e2b7bb4de44a861423210b4f9a4580b4c3b0b5
justnicksc <nickwest@fullsail.edu>
2013-10-10 23:06:06 -0400

    added variable

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -1,3 +1,5 @@
 // James West Expressions_Wacky 10/10/13
 
-
+var name = prompt ("enter your name"); //can enter full name or just first name
+var age = prompt ("enter your age"); //enter the age of the person
+var height = prompt ("enter your height")
