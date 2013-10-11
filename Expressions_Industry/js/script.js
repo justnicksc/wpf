f904c2a42707e0f9004b72154bbffa72057ca615
justnicksc <nickwest@fullsail.edu>
2013-10-10 22:23:54 -0400

    added trade in

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -2,5 +2,6 @@
 
 var fees = [.05, 389, 55,]; // sales fees 5 percent sales tax, $389 doc fee and $55 tag fee
 var carPrice = prompt("Enter the selling price of car" ,20000); // The price of the car before fees
-var downPayment = prompt ("Enter down payment" ,carPrice *.20); // the down payment has a default value of 20 percent of selling price
-
+var downPayment = prompt("Enter down payment" ,carPrice *.20); // the down payment has a default value of 20 percent of selling price
+var tradeValue = prompt("Enter trade in value" ,0);
+var amountFinanced =
