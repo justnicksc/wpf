31f3498675ed3a92b51214c68a9e3e8c7f096442
justnicksc <nickwest@fullsail.edu>
2013-10-10 21:50:46 -0400

    added comment
    added comment to explain calculation

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -9,7 +9,7 @@ var round3 = prompt("Enter round 3 score")// prompt to enter the score of your t
 var round4 = prompt("Enter round 4 score")// prompt to enter the score of your fourth round of golf
 var round5 = prompt("Enter round 5 score")// prompt to enter the score of your fifth round of golf
 
-
+// By adding the all of the scores together and dividing that total by the number of round we can get your average score
 var average = (Number(round1) + Number(round2) + Number(round3) + Number(round4) + Number(round5))/5
 
 alert ("I played 5 rounds of golf with an average score of " + average)
