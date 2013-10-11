5081d8cf5c10dba6119387cbb1abfad019d7f7c8
justnicksc <nickwest@fullsail.edu>
2013-10-10 21:47:17 -0400

    entered comment

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -6,7 +6,7 @@
 var round1 = prompt("Enter round 1 score")// enter the score of the first round of golf
 var round2 = prompt("Enter round 2 score")// enter the score of your second round of golf
 var round3 = prompt("Enter round 3 score")// enter the score of your third round of golf
-var round4 = prompt("Enter round 4 score")
+var round4 = prompt("Enter round 4 score")// enter the score of your fourth round of golf
 var round5 = prompt("Enter round 5 score")
 var average = (Number(round1) + Number(round2) + Number(round3) + Number(round4) + Number(round5))/5
 
