332d889605777aed36c73c1ffae77b392b55131c
justnicksc <nickwest@fullsail.edu>
2013-10-17 20:47:06 -0400

    added variable

--- Conditional_Personal/js/script.js
+++ Conditional_Personal/js/script.js
@@ -1,3 +1,4 @@
-/**
- * Created by nick_west on 10/17/13.
- */
+// James West Personal Conditional 10/17/13.
+
+var vacCost = prompt("Enter the Total cost of the vacation."); //This is the cost of the vacation
+
