e442d7b5cadb0dafef73e7e356f2d07dc119bef9
justnicksc <nickwest@fullsail.edu>
2013-10-17 20:48:00 -0400

    added validation for 1st prompt

--- Conditional_Personal/js/script.js
+++ Conditional_Personal/js/script.js
@@ -2,3 +2,6 @@
 
 var vacCost = prompt("Enter the Total cost of the vacation."); //This is the cost of the vacation
 
+if (vacCost == "") {
+    console.log('You cannot leave field blank.'); // this code validates entry
+}
