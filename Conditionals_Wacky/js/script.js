3f8231d8e8a3993e4668fcc1f3135d0f0add4720
justnicksc <nickwest@fullsail.edu>
2013-10-17 22:38:47 -0400

    changed prompt

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -3,7 +3,9 @@
 
 // This will calculate if there is enough water for all the people for a day
 
-var waterPerDay = 64; // this is how much a person should drink per day of water
+var waterPerDay = 64; // this is how much a person should drink per day of water in ounces
 var totalWater = prompt("Enter the total amount of water"); // user should enter the total amount of water the group has
 var totalPeople = prompt("Enter how many people are in the group"); // total amount of people in the group
-var totalWaterNeeded = waterPerDay * totalPeople;
+var totalWaterNeeded = waterPerDay * totalPeople; // This is the total water needed for the group
+
+(totalWater >= waterPerDay) "You have enough water for the whole group" : "You might want to lose dave. There is not enough water"//
