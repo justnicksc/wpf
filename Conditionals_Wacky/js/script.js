9040ba0143fe7a1a6f31de1ea51958cdcf955c80
justnicksc <nickwest@fullsail.edu>
2013-10-17 22:26:25 -0400

    added var

---
+++ Conditionals_Wacky/js/script.js
@@ -0,0 +1,6 @@
+// James West Wacky Conditional 10/17/13
+
+// This will calculate how many times your heart beats per minute and tell you if it is fast or slow
+
+var heartBeats15Sec = prompt("Count your heart beats for 15 seconds and enter here"); // user will count their heart beats for 15 seconds and enter the number
+var heartBeatsMin = heartBeats15Sec * 4;
