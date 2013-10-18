3d8cc029458e8b1c4b37f0d1f6162a737e17b695
justnicksc <nickwest@fullsail.edu>
2013-10-17 21:32:33 -0400

    added validator

--- Conditional_Industry/js/script.js
+++ Conditional_Industry/js/script.js
@@ -4,3 +4,8 @@
 
 var lastMonth = 240 // this is a preset variable for the previous month.
 var currentMonth = prompt("Enter the total sales for month.") // user will provide this information
+
+if (currentMonth == "") {
+    console.log('You cannot leave field blank.'); // this code validates entry
+     currentMonth = prompt("Enter the total cost of vacation."); // this gives the user a second chance to enter info
+}
