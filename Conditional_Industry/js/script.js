4bb01c4efaa3f06d92db015aa9377f7e6e0e4829
justnicksc <nickwest@fullsail.edu>
2013-10-17 21:35:14 -0400

    added goal prompt

--- Conditional_Industry/js/script.js
+++ Conditional_Industry/js/script.js
@@ -1,11 +1,13 @@
 //James West Industry Conditional 10/17/13
 
-//This calculates if we met our sales goal and if we sold more that last month
+//This calculates if we met our sales goal and if we sold more cars than last month
 
 var lastMonth = 240 // this is a preset variable for the previous month.
-var currentMonth = prompt("Enter the total sales for month.") // user will provide this information
+var currentMonth = prompt("Enter the total sales for month."); // user will provide this information
 
 if (currentMonth == "") {
     console.log('You cannot leave field blank.'); // this code validates entry
-     currentMonth = prompt("Enter the total cost of vacation."); // this gives the user a second chance to enter info
+     currentMonth = prompt("Enter total sales for month"); // this gives the user a second chance to enter info
 }
+
+var goal = prompt("Enter this months goal.");
