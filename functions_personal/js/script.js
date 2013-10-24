84c80820547e698716f01d75feea373362afc098
justnicksc <nickwest@fullsail.edu>
2013-10-24 19:45:53 -0400

    day 3 var

--- functions_personal/js/script.js
+++ functions_personal/js/script.js
@@ -1,7 +1,8 @@
 // James West Function Personal 10/23/13
 
-// This code will calculate my average time for running 2 miles per day for a week and see if I met my goal of lowering by at least ten seconds
+// This code will calculate my average time for running 2 miles per day for a week with 2 days rest and see if I met my goal of lowering by at least ten seconds
 
 var startingAvg = prompt("What is your starting average run time for the week"); // This is the average time for the previous week to compare to
 var day1 = prompt("Enter day 1 time"); // Time for current week day 1
 var day2 = prompt("Enter day 2 time"); // Time for day 2
+var day3 = prompt("Enter day 3 time"); // Time for day 3
