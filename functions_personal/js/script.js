f03ba49ea954650624846ffcc0a8b57d0d783bbc
justnicksc <nickwest@fullsail.edu>
2013-10-24 19:44:09 -0400

    added day 2 var

--- functions_personal/js/script.js
+++ functions_personal/js/script.js
@@ -4,3 +4,4 @@
 
 var startingAvg = prompt("What is your starting average run time for the week"); // This is the average time for the previous week to compare to
 var day1 = prompt("Enter day 1 time"); // Time for current week day 1
+var day2 = prompt("Enter day 2 time"); // Time for day 2
