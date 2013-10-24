85ca5c6ce75a2733d5faf6462432d17da6b9c5c9
justnicksc <nickwest@fullsail.edu>
2013-10-24 19:56:18 -0400

    created array for run times

--- functions_personal/js/script.js
+++ functions_personal/js/script.js
@@ -3,3 +3,5 @@
 // This code will calculate my average time for running 2 miles per day for a week with 2 days rest and see if I met my goal of lowering by at least ten seconds
 
 var startingAvg = prompt("What is your starting average run time for the week"); // This is the average time for the previous week to compare to
+var runtimes = new Array(); // current week run times
+runtimes[0] = prompt("Enter day 1 time"); // run time for current day
