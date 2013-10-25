8c540ae268f11d2a9a0c6262a96be959c77399a5
justnicksc <nickwest@fullsail.edu>
2013-10-24 23:49:45 -0400

    corrected vars

--- functions_wacky/js/script.js
+++ functions_wacky/js/script.js
@@ -6,11 +6,13 @@
 
 var week1 = prompt("Enter week 1 sales");// week 1 sales
 week1 = parseInt(week1);
-var weak2 = prompt("Enter week 1 sales");// week 1 sales
+var week2 = prompt("Enter week 1 sales");// week 1 sales
 week2 = parseInt(week2);
-var weak3 = prompt("Enter week 1 sales");// week 1 sales
+var week3 = prompt("Enter week 1 sales");// week 1 sales
 week3 = parseInt(week3);
 var week4 = prompt("Enter week 1 sales");// week 1 sales
 week4 = parseInt(week4);
 var lastMonth = prompt("Enter week 1 sales");// last months sales
 lastMonth = parseInt(lastMonth);
+
+var avgSales = avg(we)
