11a6532e5f9e9bbe2a6f8e14bfe46e20114dc042
justnicksc <nickwest@fullsail.edu>
2013-10-24 22:47:51 -0400

    added var

--- functions_industry/js/script.js
+++ functions_industry/js/script.js
@@ -1,2 +1,5 @@
 // James West Functions Industry 10/23/13
 
+// this code will determine the if there is enough wire to go around the perimeter of the building
+
+var wireNeeded = prompt("Enter the amout of wiring on hand"); //this is the amount of wire that is on hand
