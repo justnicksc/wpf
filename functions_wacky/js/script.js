1b8fe003d71f63abf1f2c79ae3ab107b6a6f48a3
justnicksc <nickwest@fullsail.edu>
2013-10-24 23:39:38 -0400

    added var

--- functions_wacky/js/script.js
+++ functions_wacky/js/script.js
@@ -5,3 +5,6 @@
 // calculate the total sale for the month and give the average week. Also tell us if we did better than last month
 
 var week1 = prompt("Enter week 1 sales");// week 1 sales
+week1 = parseInt(week1);
+var week1 = prompt("Enter week 1 sales");// week 1 sales
+week1 = parseInt(week1);
