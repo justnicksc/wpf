19cdfb1d014d351c1c30f2e459bbb0ea0c5a3a23
justnicksc <nickwest@fullsail.edu>
2013-10-24 23:57:55 -0400

    finished

--- functions_wacky/js/script.js
+++ functions_wacky/js/script.js
@@ -6,13 +6,20 @@
 
 var week1 = prompt("Enter week 1 sales");// week 1 sales
 week1 = parseInt(week1);
-var week2 = prompt("Enter week 1 sales");// week 1 sales
+var week2 = prompt("Enter week 2 sales");// week 1 sales
 week2 = parseInt(week2);
-var week3 = prompt("Enter week 1 sales");// week 1 sales
+var week3 = prompt("Enter week 3 sales");// week 1 sales
 week3 = parseInt(week3);
-var week4 = prompt("Enter week 1 sales");// week 1 sales
+var week4 = prompt("Enter week 4 sales");// week 1 sales
 week4 = parseInt(week4);
-var lastMonth = prompt("Enter week 1 sales");// last months sales
+var lastMonth = prompt("Enter last months sales");// last months sales
 lastMonth = parseInt(lastMonth);
 
-var avgSales = avg(we)
+var avgSales = avg(week1, week2, week3, week4);
+
+function avg(a, b, c, d){
+    var avgWeek = (a + b + c + d)/4;
+    return avgWeek;
+}
+
+(avgSales > lastMonth) ? console.log("You averaged " + avgSales + " dollars per week. Great job you beat last month") : console.log("You averaged " + avgSales + " dollars per week. You need to do better")
