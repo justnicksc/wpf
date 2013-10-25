a8f0c213b4a082bf1f0378b289fcd8c90854584c
justnicksc <nickwest@fullsail.edu>
2013-10-24 23:45:09 -0400

    added comments

--- functions_wacky/js/script.js
+++ functions_wacky/js/script.js
@@ -4,13 +4,13 @@
 
 // calculate the total sale for the month and give the average week. Also tell us if we did better than last month
 
-var weak1 = prompt("Enter week 1 sales");// week 1 sales
-weak1 = parseInt(weak1);
+var week1 = prompt("Enter week 1 sales");// week 1 sales
+week1 = parseInt(week1);
 var weak2 = prompt("Enter week 1 sales");// week 1 sales
-weak2 = parseInt(weak2);
+week2 = parseInt(week2);
 var weak3 = prompt("Enter week 1 sales");// week 1 sales
-week3 = parseInt(weak3);
+week3 = parseInt(week3);
 var week4 = prompt("Enter week 1 sales");// week 1 sales
 week4 = parseInt(week4);
-var lastMonth = prompt("Enter week 1 sales");// week 1 sales
+var lastMonth = prompt("Enter week 1 sales");// last months sales
 lastMonth = parseInt(lastMonth);
