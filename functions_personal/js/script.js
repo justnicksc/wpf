a892cdc620beebe3e240f341ea15d5e04d3b69e5
justnicksc <nickwest@fullsail.edu>
2013-10-24 20:39:14 -0400

    completed avg run

--- functions_personal/js/script.js
+++ functions_personal/js/script.js
@@ -11,8 +11,9 @@ runtimes[4] = prompt("Enter day 4 time"); // run time for current day
 runtimes[5] = prompt("Enter day 5 time"); // run time for current day
 
 // This calculates the average run time for the current week
-var avgrun = avgRunTime(runtimes[0], runtimes[1], runtimes[2], runtimes[3], runtimes[4])
+var avgRun = avgRunTime(runtimes[0], runtimes[1], runtimes[2], runtimes[3], runtimes[4]);
 
 function avgRunTime(d1,d2,d3,d4,d5){
-
+    var avgRun = runtimes[0] + runtimes[1] + runtimes[3] + runtimes[4]
+    return avgRun
 }
