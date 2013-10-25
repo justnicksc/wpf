7a5495e84dd9dc38253c19a3a63efbca7882ee59
justnicksc <nickwest@fullsail.edu>
2013-10-24 22:53:57 -0400

    prompt

--- functions_industry/js/script.js
+++ functions_industry/js/script.js
@@ -5,4 +5,4 @@
 var wireHand = prompt("Enter the amount of wiring on hand"); //this is the amount of wire that is on hand
 var project1 = prompt("Enter the amount of wire needed for first project") //this is the amount of wire needed
 var project2 = prompt("Enter the amount of wire needed for the third project") //this is the amount of wire needed
-var project3 = p
+var project3 = prompt
