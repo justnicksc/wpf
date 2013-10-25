e9cfc5213613dde609b51d77f21adbfdde95c332
justnicksc <nickwest@fullsail.edu>
2013-10-24 22:51:30 -0400

    entered var

--- functions_industry/js/script.js
+++ functions_industry/js/script.js
@@ -1,5 +1,6 @@
 // James West Functions Industry 10/23/13
 
-// this code will determine the if there is enough wire to go around the perimeter of the building
+// this code will determine the if there is enough wire for all 3 building projects
 
-var wireNeeded = prompt("Enter the amout of wiring on hand"); //this is the amount of wire that is on hand
+var wireHand = prompt("Enter the amount of wiring on hand"); //this is the amount of wire that is on hand
+var project1 = prompt("Enter the amount of wire needed for first project") //this is the amount of wire needed
